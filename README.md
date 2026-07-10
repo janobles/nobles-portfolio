7c95c56008f0e41b86d94861aa70e14637c4aff5
jade <jadepakyu5@gmail.com>
2026-07-10 16:40:37 +0800

    polishing deadcode lines and update background and design

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# nobles-portfolio
+# Nobles Portfolio
+
+Personal portfolio website of Jade Anthony T. Nobles. The site presents profile information, resume details, projects, practicum experience, and contact information.
+
+## Pages
+
+- Home: portfolio overview and skills
+- Resume: education, skills, certificates, and experience
+- Projects: featured academic and technical projects
+- Practicum: company overview, nature of work, rendered hours, and conclusion
+- Contact: email, phone, LinkedIn, and GitHub links
+
+## Practicum
+
+The practicum page documents the engagement at the City Government of Biñan. The practicum started on April 20, 2026. As of July 10, 2026, 400 of the required 486 hours are complete, with 86 hours remaining.
+
+## Technologies
+
+- HTML
+- CSS
+- Bootstrap
+- JavaScript
+
+## Local Development
+
+Place the project inside the XAMPP htdocs directory, start Apache, and open:
+
+    http://localhost/nobles-portfolio/nobles-portfolio/
+
+For Apache on port 8080, open:
+
+    http://localhost:8080/nobles-portfolio/nobles-portfolio/
+
+## Structure
+
+    nobles-portfolio/
+    |-- index.html
+    |-- main/
+    |   |-- contact.html
+    |   |-- practicum.html
+    |   |-- projects.html
+    |   -- resume.html
+    -- libs/
+        -- assets/
--- libs/assets/js/navbar.js
+++ libs/assets/js/navbar.js
@@ -9,55 +9,25 @@
   const root = isInnerPage ? "../" : "";
   const main = isInnerPage ? "" : "main/";
   const routes = {
-    home: `${root}index.html`,
-    resume: `${main}resume.html`,
-    projects: `${main}projects.html`,
-    practicum: `${main}practicum.html`,
-    contact: `${main}contact.html`,
-  };
-  const fallbackHeader = `
-    <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
-      <div class="container px-5">
-        <a class="navbar-brand" data-route="home" href="#">
-          <span class="fw-bolder text-primary">Nobles Portfolio</span>
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
-            <li class="nav-item"><a class="nav-link" data-route="home" href="#">Home</a></li>
-            <li class="nav-item"><a class="nav-link" data-route="resume" href="#">Resume</a></li>
-            <li class="nav-item"><a class="nav-link" data-route="projects" href="#">Projects</a></li>
-            <li class="nav-item"><a class="nav-link" data-route="practicum" href="#">Practicum</a></li>
-            <li class="nav-item"><a class="nav-link" data-route="contact" href="#">Contact</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  `;
-
-  const renderHeader = (headerMarkup) => {
-    mount.innerHTML = headerMarkup;
-
-    mount.querySelectorAll("[data-route]").forEach((link) => {
-      const route = link.dataset.route;
-      link.href = routes[route];
-    });
+    home: root + "index.html",
+    resume: main + "resume.html",
+    projects: main + "projects.html",
+    practicum: main + "practicum.html",
+    contact: main + "contact.html",
   };
 
-  renderHeader(fallbackHeader);
-
-  fetch(`${root}libs/partials/header.html`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Header partial could not be loaded.");
-      }
-
-      return response.text();
-    })
-    .then(renderHeader)
-    .catch(() => {
-      renderHeader(fallbackHeader);
-    });
+  mount.innerHTML = [
+    '<nav class="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">',
+    '<div class="container px-5">',
+    '<a class="navbar-brand" href="' + routes.home + '"><span class="fw-bolder text-primary">Nobles Portfolio</span></a>',
+    '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>',
+    '<div class="collapse navbar-collapse" id="navbarSupportedContent">',
+    '<ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">',
+    '<li class="nav-item"><a class="nav-link" href="' + routes.home + '">Home</a></li>',
+    '<li class="nav-item"><a class="nav-link" href="' + routes.resume + '">Resume</a></li>',
+    '<li class="nav-item"><a class="nav-link" href="' + routes.projects + '">Projects</a></li>',
+    '<li class="nav-item"><a class="nav-link" href="' + routes.practicum + '">Practicum</a></li>',
+    '<li class="nav-item"><a class="nav-link" href="' + routes.contact + '">Contact</a></li>',
+    '</ul></div></div></nav>',
+  ].join("");
 })();
